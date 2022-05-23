2446a182fe2bcea1e95b90d4400a6203f1b5602d
Adam Lindley <adamlindley98@gmail.com>
2022-05-23 16:07:53 +0100

    Create operate function. Takes an operator and 2 numbers and executes the operator using the 2 numbers.

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-function add(a,b){
+//main functions
+
+function add(a, b){
     return a + b;
 }
 
-function subtract(a,b){
+function subtract(a, b){
     return a - b;
 }
 
-function multiply(a,b){
+function multiply(a, b){
     return a * b;
 }
 
-function divide(a,b){
+function divide(a, b){
     return a / b;
+}
+
+function operate(operator, a, b){
+    return operator(a, b);
 }
